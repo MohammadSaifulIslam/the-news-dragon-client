637a18e5463f2e8794441140bb8c90367c9e33f0
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-04-19 19:28:08 +0600

    header compo, moment js and react marque for latest news function added

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
+import Marquee from "react-fast-marquee";
+import { Button, Container } from 'react-bootstrap';
 
 const Headeer = () => {
     return (
-        <div>
-            This is header
+        <Container>
+            <div className='text-center mt-5'>
+            <img src={logo} alt="" />
+            <p><small className='text-secondary mt-2'>Journalism Without Fear or Favour</small></p>
+            <p className='mt-0'>{moment().format("dddd, MMMM D, YYYY")}</p>
+            <div className='d-flex bg-light mb-4 p-2'>
+            <Button variant="danger">Latest</Button>
+            <Marquee>
+            Match Highlights: Germany vs Spain — as it happened   ! Match Highlights: Germany vs Spain — as it happened   ! Match Highlights: Germany vs Spain — as it happened   !
+            </Marquee>
+            </div>
         </div>
+        </Container>
     );
 };
 
