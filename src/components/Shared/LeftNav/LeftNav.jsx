62f96490df2eaa03740e40898f7dea4f6620dab9
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-04-20 09:09:22 +0600

    fetch category data from server and show in ui function added

--- src/components/Shared/LeftNav/LeftNav.jsx
+++ src/components/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link,NavLink } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/categories')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+        .catch(error => console.error(error))
+    }, [])
     return (
         <div>
-            left nav
+           <h3>All Caterogy</h3>
+           <div >
+            {
+                categories.map(category => <p
+                key={category.id}
+                className='text-light ms-3 mt-3'
+                >
+                    <NavLink  to={`category/${category.id}`} className={` text-secondary text-decoration-none`}>{category.name}</NavLink>
+                </p>)
+            }
+           </div>
         </div>
     );
 };
